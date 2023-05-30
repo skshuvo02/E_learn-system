b8593b693ae47685d0e6a3d5753dd5a3106f148e
SK Shuvo <shuvochakishanto@gmail.com>
2023-05-30 11:48:16 +0600

    modified

--- youtubeApi/uapi.js
+++ youtubeApi/uapi.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
     var API_KEY = "AIzaSyCSWyLv-vehCKYpqcgIJ6w1uKI2OBVggE8"
 
+    var video = ''
+
     $("form").submit(function (event) {
         event.preventDefault()
-        alert("form is submitted")
 
         var search = $("#form").val()
 
@@ -13,8 +14,19 @@ $(document).ready(function(){
     function videoSearch(key,  search, maxResults) {
         // jq get req
         $.get("https://www.googleapis.com/youtube/v3/search?key=" + key 
-        + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search,function(data){})
-        console.log(data)
+        + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search,function(data){
+            console.log(data)
+
+            data.items.forEach(item => {
+                video = `
+                
+                <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+                
+                `
+                $("#videos").append(video)
+            });
+        })
+        
     }
 })
 console.log("ok")
